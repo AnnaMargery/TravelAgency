2ab6d91cbdace9ffbd29408063a20208a0c05f72
Anna Margery <anna.szewc@gmail.com>
2023-11-25 16:40:10 +0100

    User Role Added

--- src/main/java/com/example/travelagency/model/UserRole.java
+++ src/main/java/com/example/travelagency/model/UserRole.java
@@ -1,9 +1,6 @@
 package com.example.travelagency.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -13,6 +10,7 @@ public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
     private String name;
 
 
--- src/main/java/com/example/travelagency/service/UserService.java
+++ src/main/java/com/example/travelagency/service/UserService.java
@@ -5,13 +5,15 @@ import com.example.travelagency.model.UserRole;
 import com.example.travelagency.repository.UserRepository;
 import com.example.travelagency.repository.UserRoleRepository;
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -23,29 +25,47 @@ public class UserService implements UserDetailsService {
     UserRepository userRepo;
     @Autowired
     UserRoleRepository userRoleRepo;
+    private final PasswordEncoder passwordEncoder;
 
-
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PostConstruct
-    public void init() {
+    public void initRoles() {
 
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        UserRole adminRole = new UserRole();
+        adminRole.setName("ADMIN");
+
+        UserRole adminSaved = userRoleRepo.save(adminRole);
+
+        UserModel admin = UserModel.builder()
+                .password(passwordEncoder.encode("admin"))
+                .username("admin")
+                .email("admin@admin.com")
+                .userRoles(Set.of(adminSaved))
+                .build();
+        userRepo.save(admin);
 
         UserRole userRole = new UserRole();
-        userRole.setName("ADMIN");
+        userRole.setName("USER");
 
-        UserRole saved = userRoleRepo.save(userRole);
+        UserRole userSaved = userRoleRepo.save(userRole);
 
         UserModel user = UserModel.builder()
-                .password(passwordEncoder.encode("test"))
-                .username("test")
-                .email("test@test.com")
-                .userRoles(Set.of(saved))
+                .password(passwordEncoder.encode("user"))
+                .username("user")
+                .email("user@user.com")
+                .userRoles(Set.of(userSaved))
                 .build();
-
         userRepo.save(user);
 
     }
+
+
+
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel userModel = userRepo.findByUsernameOrEmail(username, username).get();
