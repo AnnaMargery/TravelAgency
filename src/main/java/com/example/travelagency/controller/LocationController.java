8df7fdfd969555bfc38e577bd81afb0ea4be01fc
Anna Margery <anna.szewc@gmail.com>
2023-11-28 01:42:16 +0100

    Feature TAA-1.1/1cd
    Fixed addLocationsMethod in Service, added methods co Controller-

--- src/main/java/com/example/travelagency/controller/LocationController.java
+++ src/main/java/com/example/travelagency/controller/LocationController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,14 +23,46 @@ public class LocationController {
         return ResponseEntity.ok(locationList);
     }
 
-
-    @GetMapping("/{continent}")
+    @GetMapping("/continent/{continent}")
     public ResponseEntity<List<LocationModel>> getLocationsByContinent(@PathVariable String continent) {
         try {
-           List<LocationModel> locationList = locationService.getLocationListByContinent(continent);
+            List<LocationModel> locationList = locationService.getLocationListByContinent(continent);
             return ResponseEntity.ok(locationList);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @GetMapping("/country/{country}")
+    public ResponseEntity<List<LocationModel>> getLocationsByCountry(@PathVariable String country) {
+        try {
+            List<LocationModel> locationModelList = locationService.getLocationListByCountry(country);
+            return ResponseEntity.ok(locationModelList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/city/{city}")
+    public ResponseEntity<List<LocationModel>> getLocationsByCity(@PathVariable String city) {
+        try {
+            List<LocationModel> locationModelList = locationService.getLocationListByCity(city);
+            return ResponseEntity.ok(locationModelList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // NIE MOGE JEJ PRZETESTOWAC, BO PRZEZ TO SECURITY NIE MOGE OBEJSC POSTMANA.
+    @PostMapping
+    public ResponseEntity<LocationModel> addLocation(@RequestBody LocationModel locationToAdd){
+        try {
+            LocationModel location = locationService.addLocation(locationToAdd);
+            return ResponseEntity.ok(location);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    //TODO to implement: methods:  delete/ edit
 }
