7d766d3940a0d76ed6aed55204564d0e891e7141
think-gold <118820315+think-gold@users.noreply.github.com>
2023-12-13 16:31:26 +0100

    test with TestEntityManager passed

--- src/test/java/com/example/travelagency/repository/TripRepositoryTest.java
+++ src/test/java/com/example/travelagency/repository/TripRepositoryTest.java
@@ -1,72 +1,74 @@
 package com.example.travelagency.repository;
-
 import com.example.travelagency.model.TripModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
+
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
 class TripRepositoryTest {
-    @Mock
+    @Autowired
     private TripRepository tripRepository;
+@Autowired
+    TestEntityManager testEntityManager;
 
-    private TripModel tripModel;
-
-        @BeforeEach
-    void setUp() {
-        TripModel trip = new TripModel();
-        trip.setId(111l);
-        trip.setStartDate(new Date(2024, 06, 03));
-        trip.setEndDate(new Date(2024, 06, 04));
-        trip.setNumberOfPlaces(20);
-        trip.setPriceForAdult(100d);
-        trip.setPriceForChild(50d);
-        tripRepository.save(trip);
-    }
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.initMocks(this);
+//        TripModel trip = new TripModel();
+//        trip.setId(111l);
+//        trip.setStartDate(new Date(2024, 06, 03));
+//        trip.setEndDate(new Date(2024, 06, 04));
+//        trip.setNumberOfPlaces(20);
+//        trip.setPriceForAdult(100d);
+//        trip.setPriceForChild(50d);
+//        tripRepository.save(trip);
+//    }
 
     @Test
     void givenTripObject_whenSave_thenReturnSavedTrip() {
         //given
-        tripModel = new TripModel();
-        tripModel.setId(110l);
-        tripModel.setStartDate(new Date(2024, 06, 03));
-        tripModel.setEndDate(new Date(2024, 06, 04));
+        TripModel tripModel = new TripModel();
         tripModel.setDuration(1l);
         tripModel.setNumberOfPlaces(30);
         tripModel.setPriceForAdult(150d);
         tripModel.setPriceForChild(50d);
         //when
-        tripRepository.save(tripModel);
+        TripModel save = tripRepository.save(tripModel);
 
         //then
+        assertThat(testEntityManager.find(TripModel.class, save.getId())).isEqualTo(tripModel);
         assertThat(tripModel).isNotNull();
         assertThat(tripModel.getPriceForAdult()).isEqualTo(150d);
 
     }
-//    @Test
-//    void returnAllTrips() {
-//        //given
-//        tripModel = new TripModel();
-//        tripModel.setId(110l);
-//        tripModel.setStartDate(new Date(2024, 06, 03));
-//        tripModel.setEndDate(new Date(2024, 06, 04));
-//        tripModel.setDuration(1l);
-//        tripModel.setNumberOfPlaces(30);
-//        tripModel.setPriceForAdult(150d);
-//        tripModel.setPriceForChild(50d);
-//        tripRepository.save(tripModel);
-//        //when
-//        List<TripModel> allTrips = tripRepository.findAll();
-//        //then
-//        assertThat(allTrips).hasSize(2);
-//    }
+
+    @Test
+    void returnAllTrips() {
+        //given
+        TripModel tripModel = new TripModel();
+        tripModel.setId(110l);
+
+        tripModel.setDuration(1l);
+        tripModel.setNumberOfPlaces(30);
+        tripModel.setPriceForAdult(150d);
+        tripModel.setPriceForChild(50d);
+        tripRepository.save(tripModel);
+        //when
+        List<TripModel> allTrips = tripRepository.findAll();
+        //then
+        assertThat(allTrips).hasSize(2);
+    }
 }
