d2732e8304c570f8c0748daee53f9394b03e2a3d
think-gold <118820315+think-gold@users.noreply.github.com>
2023-12-14 10:03:05 +0100

    testTripRepository: tests for all methods passed

--- src/test/java/com/example/travelagency/repository/TripRepositoryTest.java
+++ src/test/java/com/example/travelagency/repository/TripRepositoryTest.java
@@ -1,18 +1,11 @@
 package com.example.travelagency.repository;
 
+import com.example.travelagency.model.FoodOption;
 import com.example.travelagency.model.TripModel;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -71,4 +64,59 @@ class TripRepositoryTest {
         //then
         assertThat(allTrips).hasSize(9);
     }
+
+    @Test
+    void returnTripsByContinent(){
+        //when
+        List<TripModel> tripsInEurope = tripRepository.findTripModelByHotelAddressLocationContinent("Europe");
+        //then
+        assertThat(tripsInEurope).hasSize(8);
+    }
+
+    @Test
+    void findTripByCountry(){
+        //when
+        List<TripModel> tripsInChina = tripRepository.findTripModelByHotelAddressLocationCountry("China");
+        //then
+        assertThat(tripsInChina).hasSize(1);
+    }
+
+    @Test
+    void returnTripsByFoodOption(){
+        //given
+        FoodOption foodOption = FoodOption.BB;
+        //when
+        List<TripModel> tripsWithBB = tripRepository.findTripModelByFoodOption(foodOption);
+        //then
+        assertThat(tripsWithBB).hasSize(2);
+    }
+
+    @Test
+    void returnPromotedTrips(){
+        //when
+        List<TripModel> promotedTrips = tripRepository.findByPromotedIsTrue();
+        //then
+        assertThat(promotedTrips).hasSize(7);
+    }
+
+    @Test
+    void returnTripsByOptions(){
+        //given
+        Integer hotelStandard = 5;
+        FoodOption foodOption = FoodOption.BB;
+        String country = "France";
+        String continent = "Europe";
+        //when
+        List<TripModel> selectedTrips = tripRepository.findSelectedTrip(hotelStandard, foodOption, continent, country);
+        //then
+        assertThat(selectedTrips).hasSize(1);
+    }
+    @Test
+    void returnTripsByStandardContinentAndCountry(){
+        //when
+        List<TripModel> selectedTripWithoutFood = tripRepository.findSelectedTripWithoutFood(3, "Europe", "United Kingdom");
+        //then
+        assertThat(selectedTripWithoutFood).hasSize(2);
+    }
+
 }
